2ef9beb6a4e31d5b04b8f6434264c578e91c6271
Anton <kazarian.anton.wp@mail.ru>
2025-03-11 22:38:02 +0300

    :white_check_mark:added the layout of the home page

--- src/components/HomePageText.tsx
+++ src/components/HomePageText.tsx
@@ -1,8 +1,8 @@
 export const HomePageText = () => {
   return (
     <div className="title">
-      <p className="title__new-site">Новая страница</p>
-      <p className="title__phrase">Выберите подходящий шаблон и адаптируйте его под ваши цели.</p>
+      <h1 className="title__new-site">Новая страница</h1>
+      <h2 className="title__phrase">Выберите подходящий шаблон и адаптируйте его под ваши цели.</h2>
     </div>
   );
 };
