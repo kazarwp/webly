9d3182448f4427414ba558a5a495c1aed9c976e2
Anton <kazarian.anton.wp@mail.ru>
2025-03-18 11:55:00 +0300

    :white_check_mark:added the EditBar component and redux store

---
+++ src/components/EditBar.tsx
@@ -0,0 +1,21 @@
+interface IEditBar {
+  handleOpenPopup: () => void;
+}
+
+export const EditBar = ({ handleOpenPopup }: IEditBar) => {
+  return (
+    <nav className="edit-menu">
+      <ul className="edit-menu__list">
+        <li className="edit-menu__element" onClick={() => handleOpenPopup()}>
+          Меню
+        </li>
+        <li className="edit-menu__element">Обложка</li>
+        <li className="edit-menu__element">Заголовок</li>
+        <li className="edit-menu__element">Текст</li>
+        <li className="edit-menu__element">Изображение</li>
+        <li className="edit-menu__element">Кнопка</li>
+        <li className="edit-menu__element">Подвал</li>
+      </ul>
+    </nav>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import { Provider } from "react-redux";
 
 import "./index.css";
+import { store } from "./store.ts";
 
 import { HomePage } from "./pages/HomePage.tsx";
+import { EditPage } from "./pages/EditPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/edit" element={<EditPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
