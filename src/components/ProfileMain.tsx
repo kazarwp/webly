6eb4cfac5b027cb18ef7c41a3351a591bbd654a1
Vladislav Miroshnitchenko <mefyqoje@mail.ru>
2025-03-25 19:53:17 +0300

    upgrade ProfilePage

--- src/components/ProfileMain.tsx
+++ src/components/ProfileMain.tsx
@@ -1,9 +1,23 @@
-import profileImg from "../assets/profile_logo.svg";
+import logoImage from "../assets/profile_logo.svg";
 
 export const ProfileMain = () => {
   return (
-    <div className="card__img-container">
-        <img className="card__img" src={profileImg} alt="Лого карточки" />
+    <div className="profile-container">
+      {/* Логотип */}
+      <img src={logoImage} alt="Логотип" className="logo" />
+      
+      {/* Заголовки */}
+      <h2 className="profile-title">Профиль</h2>
+      <p className="profile-username">@Имяпользователя</p>
+      <p className="page-section-title">Ваша страница</p>
+
+      {/* Карточка страницы (только этот блок с рамкой) */}
+      <div className="page-card">
+        <p className="page-name">@Имястраницы</p>
+        <hr />
+        <button className="edit-button">Редактировать страницу</button>
+        <p className="page-link">@http://ccылкастраницы</p>
+      </div>
     </div>
   );
 };
